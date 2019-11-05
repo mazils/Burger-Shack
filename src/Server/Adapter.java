37c320d8c7a10912203b61341bb316f8be747b56
ThePandoR <sasa9993@gmail.com>
2019-11-05 13:00:21 +0100

    IServer interface name changed to RemoteServer Adapter class created ArrayList provided class added ListADT provided interface added

---
+++ src/Server/Adapter.java
@@ -0,0 +1,66 @@
+package Server;
+
+public class Adapter implements ListADT{
+
+    private ArrayList arrayList;
+
+    public Adapter()
+    {
+        arrayList = new ArrayList();
+    }
+
+    @Override
+    public void add(int index, Object element) {
+        arrayList.add(index, element);
+    }
+
+    @Override
+    public void add(Object element) {
+        arrayList.add(element);
+    }
+
+    @Override
+    public void set(int index, Object element) {
+        arrayList.set(index, element);
+    }
+
+    @Override
+    public Object get(int index) {
+        return arrayList.get(index);
+    }
+
+    @Override
+    public Object remove(int index) {
+        return arrayList.remove(index);
+    }
+
+    @Override
+    public Object remove(Object element) {
+        return arrayList.remove(element);
+    }
+
+    @Override
+    public int indexOf(Object element) {
+        return arrayList.indexOf(element);
+    }
+
+    @Override
+    public boolean contains(Object element) {
+        return arrayList.contains(element);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return arrayList.isEmpty();
+    }
+
+    @Override
+    public boolean isFull() {
+        return arrayList.isFull();
+    }
+
+    @Override
+    public int size() {
+        return arrayList.size();
+    }
+}
